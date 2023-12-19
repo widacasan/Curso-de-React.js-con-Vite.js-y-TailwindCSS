1fc55df850db9bc5922dabd65ce4c8def911629c
William David Camacho Sanchez <will@MacBook-Air-de-William.local>
2023-12-18 19:02:32 -0500

    Componente de Card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,13 @@
+import Card from "../../Components/Card"
 import Layout from "../../Components/Layout"
+
 function Home() {
 
 
   return (
     <Layout>
       Home
+      <Card />
     </Layout>
   )
 }
