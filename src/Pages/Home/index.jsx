b668201e2bc4abad530c578248e45ed63caa5625
William David Camacho Sanchez <will@MacBook-Air-de-William.local>
2023-12-20 17:38:44 -0500

    Consumo de API para pintar cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,26 @@
-import Card from "../../Components/Card"
-import Layout from "../../Components/Layout"
+import { useState, useEffect } from 'react'
+import Layout from '../../Components/Layout'
+import Card from '../../Components/Card'
 
 function Home() {
+  const [items, setItems] = useState(null)
 
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
 
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
